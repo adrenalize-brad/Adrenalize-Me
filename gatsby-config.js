44d3b28a9e19d956d973f436dbdcc4c20e9593a3
adrenalize-brad <brad@adrenalizedigital.ca>
2021-04-10 10:38:00 -0600

    Add Gatsby-Netlify Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         precachePages: [`/*`],
       },
     },
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-gatsby-cloud`,
     {
       resolve: 'gatsby-source-graphcms',
