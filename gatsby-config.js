679f6a913fbfd980bbaf2e955196bcc3ffa111a6
adrenalize-brad <brad@adrenalizedigital.ca>
2021-04-12 14:04:39 -0600

    modify cache-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,7 @@ module.exports = {
         icon:'src/assets/images/adrenalize-logo.png'
       },
     },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/*`,'/*/*'],
-      },
-    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     {
       resolve: 'gatsby-source-graphcms',
